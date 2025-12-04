980effbebc30362f787830923cf2c3a6b952182c
Benjamin Sugden <benjamin.sugden@swisstopo.ch>
2025-12-04 15:30:18 +0100

    Cognito client
    Cognito client to create/delete user groups.
    Extend organization model to create/delete groups on save.
    
    Change to organization model that id cannot be updated.

---
+++ app/cognito/utils/client.py
@@ -0,0 +1,40 @@
+from boto3 import client
+
+from django.conf import settings
+
+
+class Client:
+    """ A low level client for managing cognito users and groups.
+    """
+
+    def __init__(self) -> None:
+        self.endpoint_url = settings.COGNITO_ENDPOINT_URL
+        self.user_pool_id = settings.COGNITO_POOL_ID
+        self.managed_flag_name = settings.COGNITO_MANAGED_FLAG_NAME
+        self.client = client("cognito-idp", endpoint_url=self.endpoint_url)
+
+    def create_group(self, name: str) -> bool:
+        """Create a new cognito user group.
+
+        Returns False if the group already exists.
+        """
+        try:
+            self.client.create_group(
+                GroupName=name,
+                UserPoolId=self.user_pool_id,
+                Description="Managed by service-control",
+            )
+        except self.client.exceptions.GroupExistsException as e:
+            return False
+        return True
+
+    def delete_group(self, name: str) -> bool:
+        """Delete the cognito user group.
+
+        Returns False id the group does not exist.
+        """
+        try:
+            self.client.delete_group(GroupName=name, UserPoolId=self.user_pool_id)
+        except self.client.exceptions.ResourceNotFoundException:
+            return False
+        return True
---
+++ app/tests/organization/test_organization_model.py
@@ -0,0 +1,219 @@
+from unittest.mock import patch
+
+from organization.models import Organization
+from pytest import raises
+
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+
+
+@patch('organization.models.Client')
+def test_object_stored_as_expected_for_valid_input(client, db):
+    organization_in = {
+        "organization_id": "ch.bafu",
+        "name_de": "Bundesamt für Umwelt",
+        "name_fr": "Office fédéral de l'environnement",
+        "name_en": "Federal Office for the Environment",
+        "name_it": "Ufficio federale dell'ambiente",
+        "name_rm": "Uffizi federal per l'ambient",
+        "acronym_de": "BAFU",
+        "acronym_fr": "OFEV",
+        "acronym_en": "FOEN",
+        "acronym_it": "UFAM",
+        "acronym_rm": "UFAM",
+    }
+    Organization.objects.create(**organization_in)
+
+    organizations = Organization.objects.all()
+
+    assert len(organizations) == 1
+
+    actual = Organization.objects.last()
+    assert organization_in["name_de"] == actual.name_de
+    assert organization_in["name_fr"] == actual.name_fr
+    assert organization_in["name_en"] == actual.name_en
+    assert organization_in["name_it"] == actual.name_it
+    assert organization_in["name_rm"] == actual.name_rm
+
+    assert organization_in["acronym_de"] == actual.acronym_de
+    assert organization_in["acronym_fr"] == actual.acronym_fr
+    assert organization_in["acronym_en"] == actual.acronym_en
+    assert organization_in["acronym_it"] == actual.acronym_it
+    assert organization_in["acronym_rm"] == actual.acronym_rm
+
+    assert client.return_value.create_group.called
+
+
+@patch('organization.models.Client')
+def test_object_created_in_db_with_optional_fields_null(client, db):
+    organization_in = {
+        "organization_id": "ch.bafu",
+        "name_de": "Bundesamt für Umwelt",
+        "name_fr": "Office fédéral de l'environnement",
+        "name_en": "Federal Office for the Environment",
+        "name_it": None,
+        "name_rm": None,
+        "acronym_de": "BAFU",
+        "acronym_fr": "OFEV",
+        "acronym_en": "FOEN",
+        "acronym_it": None,
+        "acronym_rm": None,
+    }
+    Organization.objects.create(**organization_in)
+
+    organizations = Organization.objects.all()
+
+    assert len(organizations) == 1
+
+    actual = Organization.objects.last()
+    assert actual.organization_id == organization_in["organization_id"]
+
+    assert actual.name_de == organization_in["name_de"]
+    assert actual.name_fr == organization_in["name_fr"]
+    assert actual.name_en == organization_in["name_en"]
+    assert actual.name_it == organization_in["name_it"]
+    assert actual.name_rm == organization_in["name_rm"]
+
+    assert actual.acronym_de == organization_in["acronym_de"]
+    assert actual.acronym_fr == organization_in["acronym_fr"]
+    assert actual.acronym_en == organization_in["acronym_en"]
+    assert actual.acronym_it == organization_in["acronym_it"]
+    assert actual.acronym_rm == organization_in["acronym_rm"]
+
+    assert client.return_value.create_group.called
+
+
+def test_raises_exception_when_creating_db_object_with_mandatory_field_null(db):
+    with raises(ValidationError):
+        Organization.objects.create(name_de=None)
+
+
+def test_form_valid_for_blank_optional_field(db):
+
+    class OrganizationForm(ModelForm):
+
+        class Meta:
+            model = Organization
+            fields = "__all__"
+
+    data = {
+        "organization_id": "ch.bafu",
+        "name_de": "Bundesamt für Umwelt",
+        "name_fr": "Office fédéral de l'environnement",
+        "name_en": "Federal Office for the Environment",
+        "acronym_de": "BAFU",
+        "acronym_fr": "OFEV",
+        "acronym_en": "FOEN",
+    }
+    form = OrganizationForm(data)
+
+    assert form.is_valid() is True
+
+
+def test_form_invalid_for_blank_mandatory_field(db):
+
+    class OrganizationForm(ModelForm):
+
+        class Meta:
+            model = Organization
+            fields = "__all__"
+
+    data = {
+        "organization_id": "ch.bafu",
+        "name_de": "Bundesamt für Umwelt",
+        "name_fr": "Office fédéral de l'environnement",
+        "name_en": "Federal Office for the Environment",
+        "acronym_de": "BAFU",
+        "acronym_fr": "OFEV",
+        "acronym_en": "",  # empty but mandatory field
+    }
+    form = OrganizationForm(data)
+
+    assert form.is_valid() is False
+
+
+@patch('organization.models.Client')
+def test_raises_exception_for_existing_slug(client, db):
+    Organization.objects.create(
+        organization_id="ch.bafu",
+        name_de="Bundesamt für Umwelt",
+        name_fr="Office fédéral de l'environnement",
+        name_en="Federal Office for the Environment",
+        acronym_de="BAFU",
+        acronym_fr="OFEV",
+        acronym_en="FOEN",
+    )
+    with raises(ValidationError):
+        Organization.objects.create(
+            organization_id="ch.bafu",
+            name_de="XXX",
+            name_fr="YYY",
+            name_en="ZZZ",
+            acronym_de="xxx",
+            acronym_fr="yyy",
+            acronym_en="zzz",
+        )
+
+    assert Organization.objects.count() == 1
+    assert client.return_value.create_group.call_count == 1
+
+
+@patch('organization.models.Client')
+def test_save_updates_records(client, db):
+    model_fields = {
+        "organization_id": "ch.bafu",
+        "name_de": "Bundesamt für",
+        "name_fr": "Office fédéral de l'environnement",
+        "name_en": "Federal Office for the Environment",
+        "acronym_de": "BAFU",
+        "acronym_fr": "OFEV",
+        "acronym_en": "FOEN",
+    }
+    Organization.objects.create(**model_fields)
+    actual = Organization.objects.first()
+    assert actual.name_de == "Bundesamt für"
+    assert client.return_value.create_group.called
+
+    client.return_value.reset_mock()
+    actual.name_de = "Bundesamt für Umwelt"
+    actual.save()
+    updated = Organization.objects.first()
+    assert updated.name_de == 'Bundesamt für Umwelt'
+    assert client.return_value.mock_calls == []
+
+    with raises(ValidationError):
+        Organization.objects.create(
+            organization_id="ch.bafu",
+            name_de="XXX",
+            name_fr="YYY",
+            name_en="ZZZ",
+            acronym_de="xxx",
+            acronym_fr="yyy",
+            acronym_en="zzz",
+        )
+
+    assert Organization.objects.count() == 1
+    assert client.return_value.mock_calls == []
+
+
+@patch('organization.models.Client')
+def test_delete_deletes_records(client, db):
+    model_fields = {
+        "organization_id": "ch.bafu",
+        "name_de": "Bundesamt für",
+        "name_fr": "Office fédéral de l'environnement",
+        "name_en": "Federal Office for the Environment",
+        "acronym_de": "BAFU",
+        "acronym_fr": "OFEV",
+        "acronym_en": "FOEN",
+    }
+
+    Organization.objects.create(**model_fields)
+    actual = Organization.objects.first()
+
+    assert client.return_value.create_group.called
+
+    actual.delete()
+
+    assert not Organization.objects.first()
+    assert client.return_value.delete_group.called
