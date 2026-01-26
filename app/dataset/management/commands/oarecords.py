0cc726a97d10a96dcf2ffaa2d97d66cd724f5da5
Christoph Böcklin <christoph.boecklin@swisstopo.ch>
2026-01-26 10:35:51 +0100

    added export command to write oar files to s3

--- app/dataset/management/commands/oarecords.py
+++ app/dataset/management/commands/oarecords.py
@@ -6,12 +6,21 @@ from typing import Any
 
 import environ
 import requests
+import boto3
+from botocore.client import Config
 from config.settings_base import BASE_DIR
 from tinydb import Query
 from tinydb import TinyDB
 from utils.command import CustomBaseCommand
 
 env = environ.Env()
+
+OAR_PREFIX = "api/oar/v0"
+OAS_PREFIX = "api/oas/v0"
+ENV_HOSTNAME_POSTFIX = "dev.sgdi.tech"
+
+# use SSO session to start since this will be executed locally for the moment
+boto3.setup_default_session(profile_name="swisstopo-swissgeo-dev")
 
 
 class Command(CustomBaseCommand):
@@ -66,6 +75,35 @@ class Command(CustomBaseCommand):
         export = sub.add_parser(
             "export", help="Export data in the database to OGC API Records format file"
         )
+        export.add_argument(
+            "--records-bucket",
+            type=str,
+            default="oa-records-static-dev-swissgeo",
+            help="S3 Bucket to upload exported OARecords files to",
+        )
+        export.add_argument(
+            "--styles-bucket",
+            type=str,
+            default="oa-styles-static-dev-swissgeo",
+            help="S3 Bucket to upload exported OARecords styles to",
+        )
+
+        clean = sub.add_parser("clean", help="Delete static files from S3 buckets")
+        clean.add_argument(
+            "--batch-size", type=int, default=1000, help="Number of files to delete per batch"
+        )
+        clean.add_argument(
+            "--records-bucket",
+            type=str,
+            default="oa-records-static-dev-swissgeo",
+            help="S3 Bucket to delete exported OARecords files from",
+        )
+        clean.add_argument(
+            "--styles-bucket",
+            type=str,
+            default="oa-styles-static-dev-swissgeo",
+            help="S3 Bucket to delete exported OARecords styles from",
+        )
 
     def handle(self, *args: Any, **options: Any) -> None:
         # initialize temporary local file-bases databases
@@ -91,9 +129,17 @@ class Command(CustomBaseCommand):
         self.styles_db = TinyDB(BASE_DIR / "harvest" / "db_styles.json", sort_keys=True, indent=4)
         self.table_styles = self.styles_db.table("styles")
 
+        # basic S3 access configuration
+        client_access_kwargs = {
+            # "endpoint_url": s3_config['S3_ENDPOINT_URL'],
+            "region_name": "eu-west-1",
+            "config": Config(signature_version="s3v4"),
+        }
+        self.s3_client = boto3.client("s3", **client_access_kwargs)
+
         # Show parsed arguments (useful for debugging)
         if options.get("verbosity", 0) >= 2:
-            self.print("Debug: parsed args =", args)
+            self.print(f"Debug: parsed args = {json.dumps(options)}")
 
         # Handle sub-commands
         if options["command"] == "harvest":
@@ -102,10 +148,10 @@ class Command(CustomBaseCommand):
             self.do_import(*args, **options)
         if options["command"] == "merge":
             self.do_merge(*args, **options)
-        # TODO: the export command is not yet transferred since it likely will get major changes
-        # anyway with writing files to S3 instead of local filesystem
-        # if options['command'] == "export":
-        #     self.do_export(*args, **options)
+        if options["command"] == "export":
+            self.do_export(*args, **options)
+        if options["command"] == "clean":
+            self.do_clean(*args, **options)
 
     # ##########################################################################
     def do_harvest(self, *args: Any, **options: Any) -> None:
@@ -231,6 +277,14 @@ class Command(CustomBaseCommand):
             )
             # We add also an attribution link if available
             if "attributionUrl" in layersconfig_entry:
+                if not layersconfig_entry["attributionUrl"].startswith("http"):
+                    self.print_warning(
+                        f"+++ WARNING: layer {layer_id} attributionUrl does not start with http(s): {layersconfig_entry['attributionUrl']}"
+                    )
+                    layersconfig_entry["attributionUrl"] = (
+                        "http://invalid.domain/" + layersconfig_entry["attributionUrl"]
+                    )
+
                 dataset.add_link(
                     Link(
                         href=layersconfig_entry["attributionUrl"],
@@ -258,7 +312,7 @@ class Command(CustomBaseCommand):
             # Links
             # region
             dataset.add_link(
-                Link(
+                OARLink(
                     href=f"collections/swissgeo.catalog/items/{layer_id}",
                     rel="self",
                     typ="application/json",
@@ -266,11 +320,13 @@ class Command(CustomBaseCommand):
             )
 
             dataset.add_link(
-                Link(href="collections/swissgeo.catalog", rel="collection", typ="application/json")
+                OARLink(
+                    href="collections/swissgeo.catalog", rel="collection", typ="application/json"
+                )
             )
 
             dataset.add_link(
-                Link(
+                OARLink(
                     href=f"collections/{layer_id}",
                     rel="distributions",
                     typ="application/json",
@@ -306,7 +362,7 @@ class Command(CustomBaseCommand):
 
             # ----------------------------------------------------------------
             # region Merging: Distributions
-            dataset_link = Link(f"collections/swissgeo.catalog/items/{layer_id}", rel="dataset")
+            dataset_link = OARLink(f"collections/swissgeo.catalog/items/{layer_id}", rel="dataset")
 
             distribution_id = layersconfig_entry.get("serverLayerName", None) or layer_id
 
@@ -343,7 +399,7 @@ class Command(CustomBaseCommand):
                     self.table_styles.insert(style)
 
                     distribution.add_link(
-                        Link(
+                        OASLink(
                             href=f"styles/{style_id}",
                             rel="styledby",
                             typ="application/json",
@@ -394,10 +450,11 @@ class Command(CustomBaseCommand):
                                 "opacity"
                             ]
 
-                        with open(f"styles/{style_id}", "w", encoding="utf-8") as f:
-                            f.write(json.dumps(style, indent=2, ensure_ascii=False))
+                        style["id"] = style_id
+                        self.table_styles.insert(style)
+
                         wms_distribution.add_link(
-                            Link(
+                            OASLink(
                                 href=f"styles/{style_id}",
                                 rel="styledby",
                                 typ="application/json",
@@ -453,6 +510,175 @@ class Command(CustomBaseCommand):
 
     # endregion
 
+    # ##########################################################################
+    def do_export(self, *args: Any, **options: Any) -> None:
+        # region Exporting
+        self.print_success("Starting to export local data to OGC API Records files on S3...")
+
+        # setup boto3 s3 client
+        oarecords_s3_bucket = options["records_bucket"]
+        styles_s3_bucket = options["styles_bucket"]
+
+        # Export catalog records
+        self.print("Exporting catalog records...")
+        catalogCollection = Collection(_id="swissgeo.catalog", title="Swissgeo Catalog").as_dict()
+        for record in self.table_records.all():
+            self.print(f"Record ID: {record['id']}, Title: {record['properties']['title']}")
+            catalogCollection["records"].append(record)
+
+        self.s3_client.put_object(
+            Bucket=oarecords_s3_bucket,
+            Key=f"{OAR_PREFIX}/collections/swissgeo.catalog",
+            Body=json.dumps(catalogCollection, indent=2, ensure_ascii=False).encode("utf-8"),
+            ContentType="application/json",
+        )
+
+        # Export distribution collections
+        self.print(f"Generate Distributions Collections")
+        for distribution in self.distributions_db.table("collections").all():
+            self.print(f" - {OAR_PREFIX}/collections/{distribution['id']}")
+
+            self.s3_client.put_object(
+                Bucket=oarecords_s3_bucket,
+                Key=f"{OAR_PREFIX}/collections/{distribution['id']}",
+                Body=json.dumps(distribution, indent=2, ensure_ascii=False).encode("utf-8"),
+                ContentType="application/json",
+            )
+
+        # Export styles
+        self.print(f"Export styles...")
+        for style in self.table_styles.all():
+            self.print(f" - {OAS_PREFIX}/styles/{style['id']}")
+
+            self.s3_client.put_object(
+                Bucket=styles_s3_bucket,
+                Key=f"{OAS_PREFIX}/styles/{style['id']}",
+                Body=json.dumps(style, indent=2, ensure_ascii=False).encode("utf-8"),
+                ContentType="application/json",
+            )
+
+        # Write service files
+        self.print("Writing service files...")
+        geadmin_data_service = {
+            "id": "ch.admin.geo.data",
+            "links": [
+                {
+                    "href": "https://data.geo.admin.ch/api/stac/v1/",
+                    "rel": "describes",
+                    "type": "application/json",
+                    "title": "STAC API Landingpage",
+                }
+            ],
+            "properties": {"title": "Geoadmin Stac Service", "type": "ogcapi:stac"},
+        }
+        self.s3_client.put_object(
+            Bucket=oarecords_s3_bucket,
+            Key=f"{OAR_PREFIX}/collections/geoadmin.services/items/ch.admin.geo.data",
+            Body=json.dumps(geadmin_data_service, indent=2, ensure_ascii=False).encode("utf-8"),
+            ContentType="application/json",
+        )
+
+        geoadmin_wms_service = {
+            "id": "wms.geo.admin.ch",
+            "linkTemplates": [
+                {
+                    "uriTemplate": "https://wms.geo.admin.ch/?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0&FORMAT=text/xml&lang={lang}",
+                    "rel": "about",
+                    "type": "application/xml",
+                    "title": "WMS Capabilities File",
+                    "variables": {
+                        "lang": {
+                            "description": "Language",
+                            "type": "string",
+                            "default": "de",
+                            "enum": ["de", "fr", "en", "it"],
+                        }
+                    },
+                }
+            ],
+            "properties": {"title": "WMS geo.admin.ch", "type": "ogc:wms"},
+        }
+        self.s3_client.put_object(
+            Bucket=oarecords_s3_bucket,
+            Key=f"{OAR_PREFIX}/collections/geoadmin.services/items/ch.admin.geo.wms",
+            Body=json.dumps(geoadmin_wms_service, indent=2, ensure_ascii=False).encode("utf-8"),
+            ContentType="application/json",
+        )
+
+        geoadmin_wmts_service = {
+            "id": "wmts.geo.admin.ch",
+            "links": [
+                {
+                    "href": "some_url_that_does_not_exist",
+                    "rel": "service-desc",
+                    "type": "application/json",
+                },
+                {
+                    "href": "https://docs.geo.admin.ch/visualize-data/wmts.html",
+                    "rel": "service-doc",
+                    "type": "application/json",
+                },
+            ],
+            "linkTemplates": [
+                {
+                    "uriTemplate": "https://wmts.geo.admin.ch/EPSG/{EPSG}/1.0.0/WMTSCapabilities.xml",
+                    "type": "application/vnd.ogc.wmts_xml",
+                    "variables": {
+                        "EPSG": {
+                            "description": "EPSG",
+                            "format": "integer",
+                            "type": "number",
+                            "default": 2056,
+                            "enum": [2056, 21781, 4326],
+                        }
+                    },
+                    "rel": "about",
+                    "title": "WMTS Capabilities File",
+                }
+            ],
+            "properties": {"title": "WMTS geo.admin.ch", "type": "ogc:wmts"},
+        }
+        self.s3_client.put_object(
+            Bucket=oarecords_s3_bucket,
+            Key=f"{OAR_PREFIX}/collections/geoadmin.services/items/ch.admin.geo.wmts",
+            Body=json.dumps(geoadmin_wmts_service, indent=2, ensure_ascii=False).encode("utf-8"),
+            ContentType="application/json",
+        )
+
+        self.print_success("Export completed.")
+        # endregion
+
+    # ##########################################################################
+    def do_clean(self, *args: Any, **options: Any) -> None:
+        # region Cleaning
+
+        for bucket in [options["records_bucket"], options["styles_bucket"]]:
+            self.print_success(
+                f"Cleaning bucket: {bucket}. (delete files in batches of {options['batch_size']} files)..."
+            )
+
+            nr_objs = 0
+
+            kwargs = {"Bucket": bucket, "MaxKeys": options["batch_size"]}
+            result = self.s3_client.list_objects_v2(**kwargs)
+            objs = result.get("Contents", [])
+            while objs:
+                nr_objs += len(objs)
+                self.print(f"Found {len(objs)} more objects to delete in {bucket}...")
+                keys = [{"Key": obj["Key"]} for obj in objs]
+                self.print(f"Deleting {len(keys)} objects from {bucket}...")
+                for key in keys:
+                    self.print(f" - {key['Key']}")
+                self.s3_client.delete_objects(Bucket=bucket, Delete={"Objects": keys})
+                if "NextContinuationToken" in result:
+                    result = self.s3_client.list_objects_v2(
+                        ContinuationToken=result.get("NextContinuationToken"), **kwargs
+                    )
+                    objs = result.get("Contents", [])
+                else:
+                    break
+            self.print_success(f"Deleted total of {nr_objs} objects from {bucket}.")
+
 
 # ##########################################################################
 # region Class Definitions
@@ -461,6 +687,9 @@ class Command(CustomBaseCommand):
 
 class Link:
     def __init__(self, href: str, rel: str, title: str = None, typ: str = None):
+        if not href.startswith("http"):
+            raise ValueError("Link href must be a full URL starting with http:// or https://")
+
         self.href = href
         self.rel = rel
         self.title = title
@@ -473,6 +702,30 @@ class Link:
         if self.type:
             dct["type"] = self.type
         return dct
+
+
+class OARLink(Link):
+    """OAR Link
+
+    An OAR Link is a Link with correct host and prefix added to the href.
+
+    """
+
+    def __init__(self, href: str, **kwargs):
+        full_href = f"https://services.{ENV_HOSTNAME_POSTFIX}/{OAR_PREFIX}/{href}"
+        super().__init__(full_href, **kwargs)
+
+
+class OASLink(Link):
+    """OAS Link
+
+    An OAS Link is a Link with correct host and prefix added to the href.
+
+    """
+
+    def __init__(self, href: str, **kwargs):
+        full_href = f"https://services.{ENV_HOSTNAME_POSTFIX}/{OAS_PREFIX}/{href}"
+        super().__init__(full_href, **kwargs)
 
 
 class Collection:
@@ -606,7 +859,7 @@ class Distribution(Record):
         self.properties["protocol"] = self.protocol
 
         self.add_link(
-            Link(
+            OARLink(
                 href=f"collections/{self.dataset_id}/items/{self.id}",
                 rel="self",
                 typ="application/json",
@@ -620,7 +873,7 @@ class WMTSDistribution(Distribution):
     def __init__(self, title: str = "OGC Web Map Tile Service (WMTS)", **kwargs):
         super().__init__(title=title, **kwargs)
         self.add_link(
-            Link(href="collections/geoadmin.services/items/ch.admin.geo.wmts", rel="service")
+            OARLink(href="collections/geoadmin.services/items/ch.admin.geo.wmts", rel="service")
         )
 
 
@@ -630,7 +883,7 @@ class WMSDistribution(Distribution):
     def __init__(self, title: str = "OGC Web Map Service (WMS)", **kwargs):
         super().__init__(title=title, **kwargs)
         self.add_link(
-            Link(href="collections/geoadmin.services/items/ch.admin.geo.wms", rel="service")
+            OARLink(href="collections/geoadmin.services/items/ch.admin.geo.wms", rel="service")
         )
 
 
@@ -640,7 +893,7 @@ class STACDistribution(Distribution):
     def __init__(self, title: str = "STAC Download Service", **kwargs):
         super().__init__(title=title, **kwargs)
         self.add_link(
-            Link(href="collections/geoadmin.services/items/ch.admin.geo.data", rel="service")
+            OARLink(href="collections/geoadmin.services/items/ch.admin.geo.data", rel="service")
         )
 
 
