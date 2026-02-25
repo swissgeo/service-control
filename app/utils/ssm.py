f02369b10f30d60ccf44bf0912479ba6cbd764b1
Brice Schaffner <brice.schaffner@swisstopo.ch>
2026-02-25 11:23:33 +0100

    GPS-515: Fixed issue with boto3 exception when using the local client

--- app/utils/ssm.py
+++ app/utils/ssm.py
@@ -1,23 +1,19 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from boto3 import client
 
 from django.conf import settings
 
 if TYPE_CHECKING:
+    from mypy_boto3_ssm import SSMClient
     from mypy_boto3_ssm.type_defs import GetParameterResultTypeDef
-
-
-ssm_client = client("ssm")
 
 
 class SSMClient:
     """AWS client to manage ssm parameters"""
 
-    exceptions = ssm_client.exceptions
-
     def __init__(self) -> None:
-        self.client = ssm_client
+        self.client = client("ssm")
 
     def get_parameter(self, name: str) -> str:
         """Get SSM parameter value"""
@@ -28,11 +24,13 @@ class SSMClient:
         """Update SSM parameter value"""
         self.client.put_parameter(Name=name, Value=value, Overwrite=True)
 
+    @property
+    def exceptions(self) -> Any:
+        return self.client.exceptions
+
 
 class LocalClient:
     """Local parameter store client"""
-
-    exceptions = ssm_client.exceptions
 
     def __init__(self) -> None:
         pass
@@ -50,6 +48,19 @@ class LocalClient:
         value: str,
     ) -> None:
         """Update SSM parameter value"""
+
+    @property
+    def exceptions(self) -> LocalClientExceptions:
+        return LocalClientExceptions()
+
+
+class LocalClientExceptions:
+    """Local Client dummy exceptions
+
+    This is needed because of real SSM client exception that we need to catch
+    """
+
+    ValidationException = ValueError
 
 
 Client = LocalClient if settings.USE_LOCAL_SSM_STORE else SSMClient
