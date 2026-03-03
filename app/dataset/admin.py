0baf90d8d2ce250f6cc8d61abc2a84c20d98f3c1
Christoph Böcklin <christoph.boecklin@swisstopo.ch>
2026-03-03 16:50:35 +0100

    added dataset model and import mgmt command

--- app/dataset/admin.py
+++ app/dataset/admin.py
@@ -1,1 +1,26 @@
-# Register your models here.
+from typing import TYPE_CHECKING, Any
+
+from django.contrib import admin
+
+from .models import Dataset
+
+if TYPE_CHECKING:
+    from django.http.request import HttpRequest
+
+
+@admin.register(Dataset)
+class DatasetAdmin(admin.ModelAdmin):
+    """Admin View for Dataset"""
+
+    list_display = ("dataset_id", "title_short_de")
+    readonly_fields = ("created_at", "updated_at")
+
+    def get_readonly_fields(
+        self,
+        request: HttpRequest,  # noqa: ARG002 unused argument
+        obj: Any | None = None,
+    ) -> list[str] | tuple[Any, ...]:
+        if obj:
+            # Organization id cannot be updated
+            return (*self.readonly_fields, "dataset_id")
+        return self.readonly_fields
