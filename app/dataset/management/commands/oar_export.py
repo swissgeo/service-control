b56b22d32e94dba4b12d9875234a76abf44ff07b
Christoph Böcklin <christoph.boecklin@swisstopo.ch>
2026-03-05 09:25:13 +0100

    move export models into separate file

--- app/dataset/management/commands/oar_export.py
+++ app/dataset/management/commands/oar_export.py
@@ -1,18 +1,16 @@
 import json
-import pathlib
-from typing import Annotated, Any, Literal, Optional
+from typing import TYPE_CHECKING, Any
 
 import boto3
 import environ
-import requests
 from botocore.client import Config
-from config.settings_base import BASE_DIR
-from pydantic import AfterValidator, BaseModel, ConfigDict, Field
-from tinydb import Query, TinyDB
+
+from dataservice.models import Dataservice
+from dataset.export_models import LANGS, OARDataservice
 from utils.command import CustomBaseCommand
 
-from dataset.models import Dataset
-from dataservice.models import Dataservice
+if TYPE_CHECKING:
+    from django.core.management.base import CommandParser
 
 
 env = environ.Env()
@@ -31,22 +29,6 @@ SAMPLE_IDS = [
     "ch.meteoschweiz.messwerte-pollen-buche-1h",
 ]
 
-DATASETS = {}
-
-
-class Lang(BaseModel):
-    code: str
-    name: str
-    dir: str = "ltr"
-    alternate: Optional[str] = None
-
-
-LANGS = {
-    "de": Lang(code="de", name="Deutsch", dir="ltr", alternate="German"),
-    "fr": Lang(code="fr", name="Français", dir="ltr", alternate="French"),
-    "it": Lang(code="it", name="Italiano", dir="ltr", alternate="Italian"),
-    "en": Lang(code="en", name="English", dir="ltr"),
-}
 
 # For some reason geocat uses a legacy 3-letter bibliographic code
 # that we need to map to ISO 639-1 codes
@@ -72,7 +54,7 @@ class Command(CustomBaseCommand):
 
     help = "OAR management"
 
-    def add_arguments(self, parser):
+    def add_arguments(self, parser: CommandParser) -> None:
         # Call the base class method to get default arguments defined in the base class
         # (mainly 'logger')
         super().add_arguments(parser)
@@ -141,11 +123,6 @@ class Command(CustomBaseCommand):
         else:
             self.session = boto3.Session()  # pylint: disable=attribute-defined-outside-init
 
-        # initialize temporary local file-bases databases
-        # Note: usage of TinyDB is just for prototyping purposes and current state of initial development.
-        # It will likely be replaced by either directly populating Models in service-control and/or
-        # have some kind of generic harvesting mechanism that detects changes and updates only changed records.
-
         # S3 client configuration
         client_access_kwargs = {
             "region_name": "eu-central-1",
@@ -158,7 +135,7 @@ class Command(CustomBaseCommand):
         self.oastyles_s3_bucket = f"oa-styles-static-{options['target_env']}-swissgeo"
 
         # Show parsed arguments (useful for debugging)
-        if options.get("verbosity", 0) >= 2:
+        if options.get("verbosity", 0) >= 2:  # noqa: PLR2004
             self.print(f"Debug: parsed args = {json.dumps(options)}")
 
         if options["command"] == "services":
@@ -167,7 +144,7 @@ class Command(CustomBaseCommand):
             self.do_clean(*args, **options)
 
     # ##########################################################################
-    def do_export_services(self, *args: Any, **options: Any) -> None:
+    def do_export_services(self, *args: Any, **options: Any) -> None:  # noqa: ARG002
 
         services = {}
 
@@ -187,7 +164,7 @@ class Command(CustomBaseCommand):
 
         if options["upload"]:
             self.print_success("Starting to upload local OGC API Records to S3...")
-            for lang in LANGS.keys():
+            for lang in LANGS:
                 for service_id, service_record in services.items():
                     key = f"{OAR_PREFIX}/collections/geoadmin.services/items/{service_id}.{lang}"
                     self.s3_client.put_object(
@@ -201,7 +178,7 @@ class Command(CustomBaseCommand):
                     self.print(f" - {key}")
 
     # ##########################################################################
-    def do_export_landing_page(self, *args: Any, **options: Any) -> None:
+    def do_export_landing_page(self, *args: Any, **options: Any) -> None:  # noqa: ARG002
         # Landing page
         self.print("Uploading landing page...")
         landing_page = {
@@ -244,7 +221,7 @@ class Command(CustomBaseCommand):
                 "http://www.opengis.net/spec/ogcapi-records-1/1.0/conf/json",
             ]
         }
-        for lang in LANGS.keys():
+        for lang in LANGS:
             self.s3_client.put_object(
                 Bucket=self.oarecords_s3_bucket,
                 Key=f"{OAR_PREFIX}/conformance.{lang}",
@@ -299,7 +276,7 @@ class Command(CustomBaseCommand):
                 }
             ],
         }
-        for lang in LANGS.keys():
+        for lang in LANGS:
             self.s3_client.put_object(
                 Bucket=self.oarecords_s3_bucket,
                 Key=f"{OAR_PREFIX}/collections.{lang}",
@@ -311,7 +288,7 @@ class Command(CustomBaseCommand):
         # endregion
 
     # ##########################################################################
-    def do_clean(self, *args: Any, **options: Any) -> None:
+    def do_clean(self, *args: Any, **options: Any) -> None:  # noqa: ARG002
         buckets = []
         if options["records"]:
             buckets.append(self.oarecords_s3_bucket)
@@ -320,7 +297,8 @@ class Command(CustomBaseCommand):
 
         for bucket in buckets:
             self.print_success(
-                f"Cleaning bucket: {bucket}. (delete files in batches of {options['batch_size']} files)..."
+                f"Cleaning bucket: {bucket}. "
+                "(delete files in batches of {options['batch_size']} files)..."
             )
 
             nr_objs = 0
@@ -344,216 +322,3 @@ class Command(CustomBaseCommand):
                 else:
                     break
             self.print_success(f"Deleted total of {nr_objs} objects from {bucket}.")
-
-
-# ##########################################################################
-# region Class Definitions
-# ##########################################################################
-
-
-def is_url(url: str) -> str:
-    if not url.startswith("http"):
-        raise ValueError(f"{url} is not a valid URL")
-    return url
-
-
-class Link(BaseModel):
-    href: Annotated[str, AfterValidator(is_url)]
-    rel: str
-    title: Optional[str] = None
-    typ: Optional[str] = Field(default=None, serialization_alias="type")
-    hreflang: Optional[str] = None
-
-
-class TemplateLink(BaseModel):
-    uriTemplate: str
-    rel: str
-    title: Optional[str] = None
-    typ: Optional[str] = Field(default=None, serialization_alias="type")
-    variables: Optional[dict] = None
-
-
-class OARRecord(BaseModel):
-    id: str
-    links: list[Link] = Field(default_factory=list)
-    linkTemplates: list[TemplateLink] = Field(default_factory=list)
-    type: Literal["Feature"] = "Feature"
-    geometry: Optional[dict] = None
-
-
-class OARDataset(OARRecord):
-    """Dataset record
-
-    A Dataset is a Record with type="Dataset"
-
-    """
-
-    properties: dict = Field(default_factory=lambda: {"type": "Dataset"})
-    geometry: Optional[dict] = {
-        "type": "Polygon",
-        "coordinates": [
-            [[5.96, 45.82], [5.96, 47.81], [10.49, 47.81], [10.49, 45.82], [5.96, 45.82]]
-        ],
-    }
-
-    @classmethod
-    def from_dataset(self, ds: Dataset, lang: str) -> OARDataset:
-        record = OARDataset(id=ds.dataset_id)
-
-        # Set properties
-        record.properties["title"] = getattr(ds, f"title_short_{lang}", None)
-        record.properties["description"] = getattr(ds, f"description_{lang}", None)
-        # record.properties["preferredDistributionId"] = self.preferred_distribution_id
-        record.properties["language"] = LANGS[lang]
-        record.properties["languages"] = list(LANGS.values())
-        # record.properties["contacts"] = getattr(self, f"contacts_{lang}", [])
-
-        # Add links
-        # links = getattr(self, f"links_{lang}", [])
-        # for link in links:
-        #     record.links.append(link)
-
-        return record
-
-
-class OARDistribution(OARRecord):
-    """Distribution record
-
-    A Distribution is a Record with type="Distribution".
-    Every Distribution represents one specific way to access a Dataset,
-    e.g. via WMS, WMTS, STAC, etc. A Dataset can therefore have multiple
-    Distribution Records.
-
-    **Note**: We introduce here a 'protocol' property to indicate the protocol
-    used to access the distribution (e.g. OGC:WMS, OGC:WMTS, OGC:STAC).
-    This is not part of the OGC API Records standard, but it helps to identify
-    the type of distribution. We could also include this information in the 'formats'
-    property, but having a dedicated 'protocol' property makes it easier to
-    query and filter distributions based on their protocol.
-
-    """
-
-    properties: dict = Field(default_factory=lambda: {"type": "Distribution"})
-
-
-class OARDataservice(OARRecord):
-    """Service record
-
-    A Service is a Record with type="Service"
-    """
-
-    properties: dict = {}
-
-    @classmethod
-    def from_dataservice(self, ds: Dataservice, lang: str = "de") -> OARDataservice:
-        record = OARDataservice(id=ds.dataservice_id)
-
-        # Set properties
-        record.properties["title"] = getattr(ds, f"title", None)
-        record.properties["type"] = getattr(ds, f"type", None)
-
-        # Add links
-        if ds.service_doc:
-            record.links.append(
-                Link(
-                    href=ds.service_doc.href,
-                    rel=ds.service_doc.rel,
-                    typ=ds.service_doc.link_type,
-                    title=ds.service_doc.title,
-                )
-            )
-        if ds.service_desc:
-            record.links.append(
-                Link(
-                    href=ds.service_desc.href,
-                    rel=ds.service_desc.rel,
-                    typ=ds.service_desc.link_type,
-                    title=ds.service_desc.title,
-                )
-            )
-        if ds.describes:
-            record.links.append(
-                Link(
-                    href=ds.describes.href,
-                    rel=ds.describes.rel,
-                    typ=ds.describes.link_type,
-                    title=ds.describes.title,
-                )
-            )
-        for template_link in ds.templated_links.all():
-            record.linkTemplates.append(
-                TemplateLink(
-                    uriTemplate=template_link.uri_template,
-                    rel=template_link.rel,
-                    typ=template_link.link_type,
-                    title=template_link.title,
-                    variables={
-                        variable_name: variable_value
-                        for variable_name, variable_value in template_link.variables.all().values_list(
-                            "variable_name", "variable_dict"
-                        )
-                    },
-                )
-            )
-
-        return record
-
-
-class OARCollection(BaseModel):
-    """Record Collection
-
-    The record collection entity has a slightly different structure
-    than a record itself.
-    Spec: https://developer.ogc.org/api/records/index.html#tag/Collection/operation/describeCollection
-
-    Note the following:
-    /collections/{collectionId} will return a Collection with roughly the following structure:
-    {
-      "id": "string",
-      "title": "string",
-      "type": "Collection",
-      "itemType": "record",
-      "recordsArrayName": "records",
-      "records": [
-        { ... Record ... }
-      ]
-    }
-
-    /collections/{collectionId}/items will return a FeatureCollection with roughly
-    the following structure:
-    {
-      "type": "FeatureCollection",
-      "features": [
-        { ... Record ... }
-      ]
-    }
-
-    Unfortunately, the record array attribute names differ between the two endpoints.
-    For now we'll implement only the /collections/{collectionId} structure and use the
-    inline 'records' array. The /items endpoint will be implemented later once we have
-    service-control in place to serve those endpoints. We'll then remove the inline
-    'records' array from the Collection and instead add a link with rel="items" to
-    point to the /items endpoint.
-
-    """
-
-    id: str
-    title: str
-    type: str = "Collection"
-    itemType: str = "record"
-    recordsArrayName: str = "records"
-    records: list[Any] = Field(default_factory=list)
-    links: list[Link] = Field(default_factory=list)
-
-
-class Contact(BaseModel):
-    organisation: str
-    country: str
-    role: str
-    # name: Optional[str] = None
-    # position: Optional[str] = None
-    # email: Optional[str] = None
-    # phone: Optional[str] = None
-    # address: Optional[str] = None
-    # city: Optional[str] = None
-    # postal_code: Optional[str] = None
